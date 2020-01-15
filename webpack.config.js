135da09c00554a19eb4890a287e88535231d42a5
jie chen <jiechen@jiedeMacBook-Pro.local>
2020-01-15 10:04:54 +0800

    webpack开发

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,12 @@ module.exports = {
     output: {
         // filename: 'bundle.js',
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
     },
     plugins: [
         new CleanWebpackPlugin(),
