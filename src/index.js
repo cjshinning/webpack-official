8369ae8bf38d924cf17dfe40a8b31ef804c89747
jie chen <jiechen@jiedeMacBook-Pro.local>
2020-01-15 18:21:04 +0800

    webpack tree shaking

--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
-import _ from 'lodash';
-import printMe from './print';
-import './style.css';
+// import _ from 'lodash';
+// import printMe from './print';
+// import './style.css';
+import {cube} from './math';
 
 function component(){
-    var element = document.createElement('div');
-    var btn = document.createElement('button');
+    // var element = document.createElement('div');
+    // var btn = document.createElement('button');
+    var element = document.createElement('pre');
 
-    element.innerHTML = _.join(['Hello', 'webpack'], '');
+    // element.innerHTML = _.join(['Hello', 'webpack'], '');
 
-    btn.innerHTML = 'click me and check the console!';
-    btn.onclick = printMe;
+    // btn.innerHTML = 'click me and check the console!';
+    // btn.onclick = printMe;
+    element.innerHTML = [
+        'Hello webpack',
+        '5 cubed is equal to ' + cube(5)
+    ].join('\n\n');
 
-    element.appendChild(btn);
+    // element.appendChild(btn);
 
     return element;
 }
 
-// document.body.appendChild(component());
-let element = component();
-document.body.appendChild(element);
+document.body.appendChild(component());
+// let element = component();
+// document.body.appendChild(element);
 
 if(module.hot){
     module.hot.accept('./print.js', function(){
