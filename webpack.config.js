56a998b1c7fa696d352dc097fd7351ca6e6c345b
jie chen <jiechen@jiedeMacBook-Pro.local>
2020-01-15 12:26:27 +0800

    webpack模块热更新2

--- webpack.config.js
+++ webpack.config.js
@@ -27,28 +27,28 @@ module.exports = {
         }),
         new webpack.NamedModulesPlugin(),
         new webpack.HotModuleReplacementPlugin()
-    ]
-    // module: {
-    //     rules: [
-    //         {
-    //             test: /\.css$/,
-    //             use: [
-    //                 'style-loader',
-    //                 'css-loader'
-    //             ]
-    //         },
-    //         {
-    //             test: /\.(png|svg|jpg|gif)$/,
-    //             use: [
-    //                 'file-loader'
-    //             ]
-    //         },
-    //         {
-    //             test: /\.(woff|woff2|eot|ttf|otf)$/,
-    //             use: [
-    //                 'file-loader'
-    //             ]
-    //         }
-    //     ]
-    // }
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            // {
+            //     test: /\.(png|svg|jpg|gif)$/,
+            //     use: [
+            //         'file-loader'
+            //     ]
+            // },
+            // {
+            //     test: /\.(woff|woff2|eot|ttf|otf)$/,
+            //     use: [
+            //         'file-loader'
+            //     ]
+            // }
+        ]
+    }
 }
