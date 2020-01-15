bc9756a2103a073a75e28a911d0f91dbe5b20625
jie chen <jiechen@jiedeMacBook-Pro.local>
2020-01-15 10:23:15 +0800

    webpack模块热更新

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     // entry: './src/index.js',
     entry: {
         app: './src/index.js',
-        print: './src/print.js'
+        // print: './src/print.js'
     },
     output: {
         // filename: 'bundle.js',
@@ -16,13 +17,16 @@ module.exports = {
     },
     devtool: 'inline-source-map',
     devServer: {
-        contentBase: './dist'
+        contentBase: './dist',
+        hot: true
     },
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
             title: 'Output Management'
-        })
+        }),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
     ]
     // module: {
     //     rules: [
